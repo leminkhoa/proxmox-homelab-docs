d80866d877024b4232865bb762bdcfc6a4addc56
Deploy from CI <>
2025-11-16 10:09:47 +0000

    Deploy 61b571dcca967d8f2a1bd8bdab735a290874242e to gh-pages

---
+++ toc-4cd9193c.js
@@ -0,0 +1,70 @@
+// Populate the sidebar
+//
+// This is a script, and not included directly in the page, to control the total size of the book.
+// The TOC contains an entry for each page, so if each page includes a copy of the TOC,
+// the total size of the page becomes O(n**2).
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="index.html">Home</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="prerequisites.html">Prerequisites</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="proxmox_installation.html">Proxmox Installation</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="getting_started.html">Getting Started</a></span></li><li class="chapter-item expanded "><li class="part-title">Initial Setup</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="initial_setup/controller/trusted_ca_store.html">Controller Node Setup</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="initial_setup/aws/index.html">AWS Configuration</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/aws/s3_backend.html">S3 Backend</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/aws/kms_backend.html">KMS Backend</a></span></li></ol><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="initial_setup/proxmox_setup/index.html">Proxmox Setup</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/proxmox_setup/users_and_roles.html">Users and Roles</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/proxmox_setup/local_storage.html">Local Storage</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/proxmox_setup/vm_templates.html">Base VM Templates</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/proxmox_setup/base_vm_template_ubuntu_server.html">Ubuntu Server Jammy</a></span></li></ol></li></ol><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="initial_setup/vault/index.html">HashiCorp Vault</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/vault/vault_image.html">Vault Image Builder</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/vault/vault_server.html">Vault Server Deployment</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="initial_setup/vault/vault_components.html">Vault Components Setup</a></span></li></ol><li class="chapter-item expanded "><li class="part-title">Deployments</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="deploy/index.html">Overview</a></span></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="deploy/general_guidelines/index.html">General Guidelines</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="deploy/general_guidelines/build_config.html">Infrastructure Configurations</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="deploy/general_guidelines/convention_rules.html">Convention rules</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="deploy/general_guidelines/set_environment.html">Setting environment variables</a></span></li></ol><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="deploy/stacks/index.html">Stacks</a><a class="chapter-fold-toggle"><div>❱</div></a></span><ol class="section"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="deploy/stacks/spark/index.html">Spark</a></span></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="deploy/stacks/k8s/index.html">Kubernetes</a></span></li></ol><li class="chapter-item expanded "><li class="part-title">References</li></li><li class="chapter-item expanded "><span class="chapter-link-wrapper"><a href="references/deployment_modules.html">Deployment modules</a></span></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString().split('#')[0].split('?')[0];
+        if (current_page.endsWith('/')) {
+            current_page += 'index.html';
+        }
+        const links = Array.prototype.slice.call(this.querySelectorAll('a'));
+        const l = links.length;
+        for (let i = 0; i < l; ++i) {
+            const link = links[i];
+            const href = link.getAttribute('href');
+            if (href && !href.startsWith('#') && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The 'index' page is supposed to alias the first chapter in the book.
+            if (link.href === current_page
+                || i === 0
+                && path_to_root === ''
+                && current_page.endsWith('/index.html')) {
+                link.classList.add('active');
+                let parent = link.parentElement;
+                while (parent) {
+                    if (parent.tagName === 'LI' && parent.classList.contains('chapter-item')) {
+                        parent.classList.add('expanded');
+                    }
+                    parent = parent.parentElement;
+                }
+            }
+        }
+        // Track and set sidebar scroll position
+        this.addEventListener('click', e => {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        const sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via
+            // 'next/previous chapter' buttons
+            const activeSection = document.querySelector('#mdbook-sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        const sidebarAnchorToggles = document.querySelectorAll('.chapter-fold-toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(el => {
+            el.addEventListener('click', toggleSection);
+        });
+    }
+}
+window.customElements.define('mdbook-sidebar-scrollbox', MDBookSidebarScrollbox);
+
